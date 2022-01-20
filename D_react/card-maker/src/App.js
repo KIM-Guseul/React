dcca65459eff6f414e2f4b8141de18bda89c94c1
guseul <jude@1024corp.com>
2022-01-20 17:17:26 +0900

    logout add

--- D_react/card-maker/src/App.js
+++ D_react/card-maker/src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import styles from './App.module.css';
 import Login from './component/login/login';
+import Maker from './component/maker/maker';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
 
 function App({ authService }) {
   return (
     <div className={styles.app}>
-      <Login authService={authService}/>
+      <BrowserRouter>
+       <Routes>
+          <Route 
+            path = "/" 
+            element = {<Login authService={authService}/>} />
+          <Route 
+            exact path = "/maker" 
+            element = {<Maker authService={authService}/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- D_react/card-maker/src/service/auth_service.js
+++ D_react/card-maker/src/service/auth_service.js
@@ -4,13 +4,26 @@ import firebaseApp from './firebase';
 
 class AuthService {
 
+  //=== 로그인 ===
   login(providerName) {
-
     const authProvider = new firebase.auth[`${providerName}AuthProvider`]();
-
     return firebaseApp.auth().signInWithPopup(authProvider);
-
   }
+
+    //=== 로그아웃 ===
+    logout(){
+      firebase.auth().signOut();
+    }
+
+  //=== 로그인한 사용자 데이터 사용 ===
+  onAuthChange(onUserChanged) {
+    firebase.auth().onAuthStateChanged(user => {
+      onUserChanged(user);
+    });
+  }
+
+
+
 }
 
 export default AuthService;
